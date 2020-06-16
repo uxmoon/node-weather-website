f49ae3d3cb115e2a1909f5302269ad0a1526baa3
Martin Luna <martin.luna@possible.com>
2020-06-16 17:28:50 -0300

    added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
     } else if(body.error) {
       callback("Unable to find location")
     } else {
-      callback(undefined, `${body.current.weather_descriptions[0]} - It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
+      console.log(body.current)
+      callback(undefined, `${body.current.weather_descriptions[0]}. current temperature is ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`)
     }
   })
 
